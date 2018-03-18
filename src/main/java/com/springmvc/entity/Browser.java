11809525605cfe5093edde66b7e6e6d19e242ffc
wpy <1251499395@qq.com>
2018-03-18 18:43:05 +0800

    开始准备加载了浏览记录功能

--- src/main/java/com/springmvc/entity/Browser.java
+++ src/main/java/com/springmvc/entity/Browser.java
@@ -2,8 +2,8 @@ package com.springmvc.entity;
 
 public class Browser {
     public int id;//浏览用户id
-    public int notification_id;//浏览通知id
     public int broswerYear;//年
     public int broswerMonth;//月
     public int broswerDay;//日
+    public String content;
 }
--- src/main/java/com/springmvc/service/browserTable.java
+++ src/main/java/com/springmvc/service/browserTable.java
@@ -18,10 +18,10 @@ broswer_time varchar(50))
 
 create table borswerTable(
 id int NOT NULL primary key,
-notification_id int NOT NULL,
 browserYear int NOT NULL,
 browserMonth int NOT NULL,
-browserDay int NOT NULL) default charset=utf8;
+browserDay int NOT NULL,
+content varchar(100)) default charset=utf8;
  */
 	public Connection getConnection() {
 		Connection conn = null;
@@ -39,7 +39,7 @@ browserDay int NOT NULL) default charset=utf8;
 
 	public void insert(Browser browser) {
 
-		String sql = "insert into userTable(id, browserYear, browserMonth, browserDay) " +
+		String sql = "insert into userTable(id, browserYear, browserMonth, browserDay, content) " +
 				"values(?,?,?,?,?)";
 		System.out.println(sql);
 		try {
@@ -49,6 +49,7 @@ browserDay int NOT NULL) default charset=utf8;
 			ps.setInt(2, browser.broswerYear);
 			ps.setInt(3, browser.broswerMonth);
 			ps.setInt(4, browser.broswerDay);
+			ps.setString(5,browser.content);
 			int row = ps.executeUpdate();
 			ps.close();
 			conn.close();
@@ -75,7 +76,7 @@ browserDay int NOT NULL) default charset=utf8;
 				browser.broswerMonth = rs.getInt("broswerMonth");
 				browser.broswerYear = rs.getInt("broswerYear");
 				browser.id = rs.getInt("id");
-				browser.notification_id = rs.getInt("notification_id");
+				browser.content = rs.getString("content");
 				browsers.add(browser);
 			}
 			rs.close();
