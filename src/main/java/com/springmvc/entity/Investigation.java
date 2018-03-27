a4075583753d6aaaccf210cffd248b5972956cc9
wpy <1251499395@qq.com>
2018-03-27 22:47:29 +0800

    优化jsp页面的连接问题

--- src/main/java/com/springmvc/entity/Investigation.java
+++ src/main/java/com/springmvc/entity/Investigation.java
@@ -1,4 +1,9 @@
 package com.springmvc.entity;
 
 public class Investigation {
+    public int investigationId;
+    public int investigationYear;
+    public int investigationMonth;
+    public int investigationDay;
+    public int publishId;
 }
--- src/main/java/com/springmvc/service/InvestigationTable.java
+++ src/main/java/com/springmvc/service/InvestigationTable.java
@@ -1,5 +1,28 @@
 package com.springmvc.service;
 
-public class InvestigationTable {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class InvestigationTable {
+/*
+create table investigationTable(
+investigationId int NOT NULL primary key auto_increment,
+investigationYear int NOT NULL,
+investigationMonth int NOT NULL,
+investigationDay int NOT NULL,
+publishId int NOT NULL) default charset=utf8;
+*/
+    public Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/company";
+            String username = "root";
+            String password = "22003x";
+            conn = DriverManager.getConnection(url, username, password);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
